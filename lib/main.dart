f1707a3c73fb214147269d01649640739e06863c
Elizabeth Ajileye <ajioluwaseun1@gmail.com>
2024-06-22 13:22:32 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -14,6 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
+      // darkTheme: ThemeData.dark(),
+      // themeMode: ThemeMode.dark,
       home: const HomePage(),
     );
   }
@@ -21,34 +24,93 @@ class MyApp extends StatelessWidget {
 
 enum City {
   stockholm,
-   paris,
+  paris,
   tokyo,
-    nigeria,
+  nigeria,
 }
 
 typedef WeatherEmoji = String;
 
 Future<WeatherEmoji> getWeather(City city) {
- return Future.delayed(
-   const Duration(seconds: 2), 
- () => {
-  City.stockholm: '🌧',
-  City.paris: '❄',
-  City.tokyo: '🍃',
-  City.nigeria: '☀',
-  } [city]!,
+  return Future.delayed(
+    const Duration(seconds: 2),
+    () => {
+      City.stockholm: '🌧',
+      City.paris: '❄',
+      City.tokyo: '🍃',
+      City.nigeria: '☀',
+    }[city]!,
   );
 }
+
+// UI writes to this
+final currentCityProvider = StateProvider<City?>(
+  (ref) => null,
+);
+
+const unknownWeatherEmoji = '🤷‍♀';
+
+// UI reads to and reads this
+final weatherProvider = FutureProvider((ref) {
+  final city = ref.watch(currentCityProvider);
+  if (city != null) {
+    return getWeather(city);
+  } else {
+    return unknownWeatherEmoji;
+  }
+});
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    //  final display = ref.watch(weatherProvider);
+    final currentWeather = ref.watch(weatherProvider);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Weather'),
+        title: const Text('Weather'),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          currentWeather.when(
+            loading:() => const Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircularProgressIndicator(),
+            ) ,
+            error: (_, __) =>
+              const Text("Error"),
+            data: (data) => Text(
+              data.toString(),
+              style: const TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: City.values.length,
+              itemBuilder: (context, index) {
+                final city = City.values[index];
+                final isSelected = city == ref.watch(currentCityProvider);
+                return ListTile(
+                  title: Text(
+                    city.toString(),
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  trailing: isSelected ? const Icon(Icons.check) : null,
+                  onTap: () {
+                    ref.read(currentCityProvider.notifier).state = city;
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
